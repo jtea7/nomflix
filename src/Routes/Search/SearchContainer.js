f295ba809db6326598ecd160aef397e33d54fc81
Jongik Cha <hayang7896@naver.com>
2020-12-28 14:27:40 +0900

    TV, Search, Detail도 Container Presenter 형식으로 변경

---
+++ src/Routes/Search/SearchContainer.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import SearchPresenter from './SearchPresenter';
+
+/* eslint import/no-anonymous-default-export: [2, {"allowAnonymousClass": true}] */
+export default class extends React.Component {
+  state = {
+    movieResults: null,
+    tvResults: null,
+    searchTerm: '',
+    error: null,
+    loading: false,
+  };
+
+  render() {
+    const { movieResults, tvResults, searchTerm, error, loading } = this.state;
+
+    return (
+      <SearchPresenter
+        movieResults={movieResults}
+        tvResults={tvResults}
+        searchTerm={searchTerm}
+        error={error}
+        loading={loading}
+      />
+    );
+  }
+}
---
+++ src/Routes/TV/index.js
@@ -0,0 +1,5 @@
+import TVContainer from './TVContainer';
+
+//index.js 파일을 생성해야지
+//import Home from 'Routes/Home';처럼 경로만 지정하여 접근 가능하다.
+export default TVContainer;
